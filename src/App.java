3bdaa08f942ebed91c070c9f4593ddf815b05da2
RonaldEduardo <ronald29eduardo@gmail.com>
2024-05-07 23:09:46 -0300

    Teste Ronald

--- src/App.java
+++ src/App.java
@@ -5,25 +5,42 @@ public class App {
         Scanner scan = new Scanner(System.in);
         Item item = new Item();
         String[] itens = new String[0];
-        System.out.printf("""
-                Oque vc deseja fazer
+        String[][] itensValor = new String[itens.length][0];
+        boolean finalizar = false;
+        do {
+            System.out.printf("""
+                    Oque vc deseja fazer
 
-                1- Adicionar um item
-                2- Adicionar um valor item
-                3- Listar os itens
-                4- Criar no pedido
-                5- Adicionar item no pedido
-                6- Mostrar valor total
-                0- Saida
+                    1- Adicionar um item
+                    2- Adicionar um valor item
+                    3- Listar os itens
+                    4- Criar no pedido
+                    5- Adicionar item no pedido
+                    6- Mostrar valor total
+                    0- Saida
 
-                """);
-        int opcao = scan.nextInt();
-        scan.nextLine();
-        switch (opcao) {
+                    """);
+            int opcao = scan.nextInt();
+            scan.nextLine();
+
+            switch (opcao) {
                 case 1: {
                     itens = item.adicionaItem(itens, scan);
                     break;
                 }
-        }
+                case 2: {
+                    itensValor = item.preencherMatriz(itensValor, itens);
+                    itensValor = item.adicionarValorItem(itens, scan);
+                    break;
+                }
+                case 3: {
+                    item.listarItens(itensValor);
+                }
+                case 0: {
+                    finalizar = true;
+                }
+            }
+        } while (!finalizar);
+
     }
 }
--- src/Item.java
+++ src/Item.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
 public class Item {
- public String[] adicionaItem(String[] itens, Scanner scan) {
+
+  public String[] adicionaItem(String[] itens, Scanner scan) {
     String[] newItens = new String[itens.length + 1];
 
     preencherArray(itens, newItens);
@@ -17,4 +18,41 @@ public class Item {
     }
     return newItens;
   }
+
+  public String[][] adicionarValorItem(String[] item, Scanner scan) {
+    String[][] newItensValor = new String[item.length][1];
+    String[] newItens = new String[item.length];
+    System.out.println("Adicione valor a qual item?");
+    newItens = preencherArray(item, newItens);
+    newItensValor = preencherMatriz(newItensValor, newItens);
+
+    for (int i = 0; i < newItens.length; i++) {
+      System.out.println((i + 1) + " - " + newItens[i]);
+    }
+    int opcao = scan.nextInt() - 1;
+    scan.nextLine();
+
+    System.out.println("Qual o valor para item " + newItensValor[opcao][0]);
+    newItensValor[opcao][0] = scan.nextLine();
+
+    return newItensValor;
+  }
+
+  public String[][] preencherMatriz(String[][] itensValor, String[] itens) {
+    String[][] newItensValor = new String[itens.length][1];
+    for (int i = 0; i < itens.length; i++) {
+      newItensValor[i][0] = itens[i];
+    }
+    return newItensValor;
+  }
+
+  public void listarItens(String[][] itensValor) {
+    for (int i = 0; i < itensValor.length; i++) {
+      for (int j = 0; j < itensValor[i].length; j++) {
+        System.out.println(itensValor[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
+
 }
