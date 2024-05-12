e4c6a37bae2d8fa566baf235493447b8c2e40602
Gustmann <ronald29eduardo@gmail.com>
2024-05-12 00:35:17 -0300

    Refatoração adcionarItem

--- src/Item.java
+++ src/Item.java
@@ -1,16 +1,32 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Item {
 
   public String[] adicionaItem(String[] itens, Scanner scan) {
-    String[] newItens = new String[itens.length + 1];
+    // Copia a lista de itens existente para uma nova lista.
+    String[] novosItens = Arrays.copyOf(itens, itens.length);
 
-    preencherArray(itens, newItens);
+    // Flag para controle do loop. Enquanto for 'false', novos itens podem ser adicionados.
+    boolean adicionarMais = true;
 
-    System.out.println("Qual o proximo item?");
-    newItens[newItens.length - 1] = scan.nextLine();
-    return newItens;
-  }
+    while (adicionarMais) {
+        // Cria um novo array com tamanho um maior que o anterior para adicionar novo item.
+        novosItens = Arrays.copyOf(novosItens, novosItens.length + 1);
+
+        // Pede ao usuário para inserir o próximo item.
+        System.out.println("Qual o próximo item?");
+        novosItens[novosItens.length - 1] = scan.nextLine();
+
+        // Pergunta ao usuário se deseja adicionar mais itens.
+        System.out.println("Deseja adicionar mais itens? (S/N)");
+        String escolhaUsuario = scan.nextLine().toUpperCase();
+
+        // Define a condição de parada baseada na entrada do usuário.
+        adicionarMais = escolhaUsuario.equals("S") || escolhaUsuario.equals("SIM");
+    }
+    return novosItens;
+}
 
   private String[] preencherArray(String[] itens, String[] newItens) {
     for (int i = 0; i < itens.length; i++) {
@@ -38,9 +54,9 @@ public class Item {
       newValorItens[(itemEscolha - 1)] = scan.nextDouble();
       scan.nextLine();//limpar buffer
 
-      System.out.println("Continuar adicionando valor?");
+      System.out.println("Continuar adicionando valor? (S/N)");
       String opcaoEscolha = scan.nextLine();
-      preencherValores = opcaoEscolha.toUpperCase().equals("S") ? false : true;
+      preencherValores = opcaoEscolha.toUpperCase().equals("S") || opcaoEscolha.toUpperCase().equals("SIM") ? false : true;
 
     }
 
@@ -55,10 +71,17 @@ public class Item {
   }
 
   public void listarItens(String[] itens, double[] itensValor) {
-    for (int i = 0; i < itens.length; i++) {
-      System.out.print(itens[i] + " ");
-      System.out.print(itensValor[i] + " ");
-      System.out.println();
+    if (itensValor.length >= 0) {
+      for (int i = 0; i < itens.length; i++) {
+        System.out.print(itens[i] + " ");
+        System.out.println();
+      }
+    }else{
+      for (int i = 0; i < itens.length; i++) {
+        System.out.print(itens[i] + " ");
+        System.out.print(itensValor[i] + " ");
+        System.out.println();
+      }
     }
 
   }
